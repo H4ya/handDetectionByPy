433fc918aea0f412825d7f2ea1d12b52ae70403f
H4ya <H4yathegenius@gmail.com>
2025-07-03 00:53:08 +0300

    added a volume control feature

--- README.md
+++ README.md
@@ -11,5 +11,22 @@ I just got an idea, will start recording my changes here!
 
 13:24
 - added a thumbs up detector (without youtube)
-- start  using the range function, can't belive I forgot about it! life is easier now
-- messed up a little with the thumbup and ok if statements(I was trying to optimize it using range), will fix it later
+- start  using the range function, can't believe I forgot about it! life is easier now
+- messed up a little with the thumbs up and ok if statements(I was trying to optimize it using range), will fix it later
+
+18:05
+- add distinguishing right 1\left 0 hands and added it as a forth dimension in lary
+
+22:28
+- deleted unnecessary comments\code
+- fixed the thumb up state to contain right\left thumbs
+- added a range for the thumbs to be acceptable (-60,60)
+I'm proud and sleepy
+
+6/20/2025 1:50 :
+- I added if statement to check the space between 4 and 5
+- I missed up something in the lines 80-87 
+
+7/02/2025 23:05 :
+- Trying to add a sound control feature cuz it looks cool
+- (one hour later) I did it it is really cool!!!
--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 import cv2
 import mediapipe as mp
 import numpy as np
+from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
+from ctypes import cast, POINTER
+from comtypes import CLSCTX_ALL
+
+def set_volume(level):  # level: float between 0.0 and 1.0
+    devices = AudioUtilities.GetSpeakers()
+    interface = devices.Activate(IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+    volume = cast(interface, POINTER(IAudioEndpointVolume))
+    volume.SetMasterVolumeLevelScalar(level, None)
 
 mp_hands = mp.solutions.hands
 hands = mp_hands.Hands(max_num_hands=2, min_detection_confidence=0.7, min_tracking_confidence=0.5)
@@ -9,97 +18,128 @@ mp_drawing = mp.solutions.drawing_utils
 webcam = cv2.VideoCapture(0)
 
 try:
+
     while webcam.isOpened():
         success, frame = webcam.read()
         if not success:
             continue
             
-        # قلب الإطار أولاً (لتصحيح المرآة)
+        # Flip frame
         frame = cv2.flip(frame, 1)
         thumbsUp = "No thumbs |:"
 
-        # معالجة الإطار
+        # Process frame
         frame.flags.writeable = False
         results = hands.process(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
         frame.flags.writeable = True
-        # رسم المعالم والوصلات
-        if results.multi_hand_landmarks:
-            for hand_landmarks in results.multi_hand_landmarks:
-                # رسم الوصلات والمعالم
+        
+        if results.multi_hand_landmarks and results.multi_handedness:
+            for hand_idx, (hand_landmarks, handedness) in enumerate(zip(results.multi_hand_landmarks, results.multi_handedness)):
+                # Draw landmarks
                 mp_drawing.draw_landmarks(
                     frame,
                     hand_landmarks,
                     mp_hands.HAND_CONNECTIONS,
-                    mp_drawing.DrawingSpec(color=(0,0,180), thickness=2),  # لون النقاط
-                    mp_drawing.DrawingSpec(color=(225,225,225), thickness=1),  # لون الخطوط
-
-            )
-                h, w , _ = frame.shape
-                lary = np.zeros((21, 3))  # array of 21 rows and 3 cols for hand landmarks
+                    mp_drawing.DrawingSpec(color=(0,0,180), thickness=2),
+                    mp_drawing.DrawingSpec(color=(225,225,225), thickness=1),
+                )
+                
+                h, w, _ = frame.shape
+                label = handedness.classification[0].label
+                dir = 1 if label == "Right" else 0
+                
+                # Store landmarks with direction
+                lary = np.zeros((21, 4))  # [dir, x, y, z] for each landmark
                 for id, landmark in enumerate(hand_landmarks.landmark):
+                    lary[id] = [
+                        dir,  # direction (0=left, 1=right)
+                        int(landmark.x * w),  # x coordinate
+                        int(landmark.y * h),  # y coordinate
+                        float(landmark.z * w),# z coordinate
+                    ]
                     
-                    lary[id] = [int(landmark.x * w), int(landmark.y * h), float(landmark.z * w)]
+                    # Display fingertip coordinates
+                    if id % 4 == 0:
+                        cv2.putText(
+                            frame, 
+                            f"P{id}({lary[id][1]},{lary[id][2]},{round(lary[id][3],3)})", 
+                            (int(lary[id][1]), int(lary[id][2])),  # Position at landmark
+                            cv2.FONT_HERSHEY_DUPLEX, 
+                            0.4, 
+                            (211, 51, 51), 
+                            1
+                        )
                     
-                    #thumb_tip = hand_landmarks.landmark[mp_hands.HandLandmark.THUMB_TIP]
-                    
-                    okZ = lary[4][2] - lary[8][2]
-                    if(id%4==0): #يطبع لي اطراف الاصابع والرقم 0
-                        cv2.putText(frame, f"P{id}({lary[id][0]},{lary[id][1]},{round(lary[id][2],2)})", (int(lary[id][0]+10),int(lary[id][1]-10)),
-                        cv2.FONT_HERSHEY_DUPLEX, .4, (211,51,51), 1)
-                    #    print("\nYour Celsius value is {:0.2f}ºC.\n".format(answer))
-                    okX = lary[8][0] - lary[4][0]
-                    okY = lary[4][1] - lary[8][1] #functions to see if the ok symbol apply according to x, y & z
-                    if lary[4][1] != 0 and lary[8][1] != 0:#todo: re-write it
-                        if (okY <= 23) and (okY >= 0) and(okX in range(-1,8)) and (okZ >= -3) and (okZ <= 35):
-                            cv2.putText(frame, f"Hello Genius!", (20, 40),
-                                        cv2.FONT_HERSHEY_DUPLEX, 1.3, (211,51,51), 1)
-                if (int((lary[4][0] + lary[3][0] + lary[2][0])/3) in range (int(lary[4][0]-15),int(lary[4][0]+15)) and lary[4][1]<lary[3][1] and (lary[4][2] in range(-35,11))):
-                    thumbsUp = "Thumbs UP :D"
-                    
-                elif(int((lary[4][0] + lary[3][0] + lary[2][0])/3) in range (int(lary[4][0]-15),int(lary[4][0]+15)) and lary[4][1]>lary[2][1]):
-                    thumbsUp = "Thumbs down D:"
-                else:
-                    thumbsUp = "No thumbs |:"
-
-                    
-
+                    # OK gesture detection
+                    if id == 8:  # Only check once per frame
+                        okZ = lary[4][3] - lary[8][3]
+                        okX = lary[8][1] - lary[4][1]
+                        okY = lary[4][2] - lary[8][2]
                         
-                            #the rest of points will disappear if thumb tip almost/meets the index tip
+                        if (okY <= 23) and (okY >= 0) and (okX in range(-1,8)) and (okZ >= -3) and (okZ <= 35):
+                            cv2.putText(
+                                frame,
+                                "Hello Genius!",
+                                (20, 40),
+                                cv2.FONT_HERSHEY_DUPLEX, 
+                                1.3,
+                                (211, 51, 51),
+                                1
+                            )
+                #distances
+                dist_x = lary[8][1]-lary[4][1] #should be positive
+                dist_y = lary[4][2]-lary[8][2] #should be positive too 
+                dist_yP = float(dist_y/170) if dist_y<170 else 1 #the percentage from 0 to 1
+                # dir = 1 if label == "Right" else 0
 
-                    #if 4 - 8 >=30 
-
-#            for id, landmark in enumerate(hand_landmarks.landmark):
-#                    cv2.putText(frame, f"({hand_landmarks.x},{hand_landmarks.y})", (x, y),
-#                cv2.FONT_HERSHEY_DUPLEX, 1.3, (211,51,51), 1)
-
-#todo: create a sign to stop the program
-
+                if(dist_x <=0):
+                    if (dist_y <= 0):
+                        dist_yP = 0.0
+                    set_volume(dist_yP)
+                    cv2.putText(
+                        frame,
+                        f"volume is approx: {dist_yP*100}", 
+                        (15, 140),
+                        cv2.FONT_HERSHEY_DUPLEX, 
+                        0.8, 
+                        (211, 0, 0), 
+                        1
+                    )
+                    set_volume(dist_yP)
+                # Thumb state detection
+                avg_x = int((lary[4][1] + lary[3][1] )/2)
+                if ((lary[8][1]-lary[8+1][1] in range(-60,60))):
+                    if (avg_x in range(int(lary[4][1]-15), int(lary[4][1]+15)) and lary[4][2] < lary[3][2]):
+                        thumbsUp = "Thumbs UP :D"
+                        #set_volume(0.5)
+                    elif ((avg_x in range(int(lary[4][1]-15), int(lary[4][1]+15)) and lary[4][2] > lary[3][2])):
+                        thumbsUp = "Thumbs down D:"
+                        #set_volume(0)
+            # Display thumb status
+                if dir == 1:
+                    cv2.putText(
+                        frame,
+                        f"{thumbsUp} in the Right hand", 
+                        (15, 70),
+                        cv2.FONT_HERSHEY_DUPLEX, 
+                        0.8, 
+                        (211, 0, 0), 
+                        1
+                    )
+                if dir == 0:
+                    cv2.putText(
+                    frame,
+                    f"{thumbsUp} in the Left hand", 
+                    (15, 110),
+                    cv2.FONT_HERSHEY_DUPLEX, 
+                    0.8,
+                    (211, 0, 0),
+                    1
+                )
         
-                # كتابة الأرقام على النقاط
-                #h, w = frame.shape[:2]
-                #for id, landmark in enumerate(hand_landmarks.landmark):
-                #    # حساب الإحداثيات بعد قلب الصورة
-                #    x, y = int(landmark.x * w), int(landmark.y * h)
-                #    
-                #    # تعديل موقع النص ليكون أعلى النقطة قليلاً
-                #    text_y = y - 10 if y > 20 else y + 20
-                #    
-                #    cv2.putText(frame, str(id), (x, text_y), 
-                #                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,0), 1)
-
-                # عرض معدل الإطارات
-                # cv2.putText(frame, f"FPS: {int(webcam.get(cv2.CAP_PROP_FPS))}", (10, 30),
-                #             cv2.FONT_HERSHEY_SIMPLEX, 1, (211,51,51), 2)
-
-            #cv2.putText(frame, f"Hello Genius!", (20, 40),
-            #cv2.FONT_HERSHEY_DUPLEX, 1.3, (211,51,51), 1)
-        cv2.putText(frame, f"{thumbsUp}", (15,70),
-            cv2.FONT_HERSHEY_DUPLEX, .8, (211,0,0), 1)
-        
-
         cv2.imshow("Hand Landmarks", frame)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            exit()
+        if ((cv2.waitKey(1) & 0xFF == ord('q')) or (cv2.waitKey(1) & 0xFF == ord('Q'))):
+            break
 
 finally:
     webcam.release()
